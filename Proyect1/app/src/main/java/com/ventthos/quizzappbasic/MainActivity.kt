0ab0913c9434ae3c8e6fcbe3b4d7192ac56b91a9
chaan12 <eduardoezequielchanortiz@gmail.com>
2025-02-14 17:21:57 -0600

    Reajuste de Activity 1
    Mejora en el diseño

--- Proyect1/app/src/main/java/com/ventthos/quizzappbasic/MainActivity.kt
+++ Proyect1/app/src/main/java/com/ventthos/quizzappbasic/MainActivity.kt
@@ -2,20 +2,22 @@ package com.ventthos.quizzappbasic
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnJugar: Button
-    lateinit var btnOpciones: Button
+
+    private lateinit var btnJugar: CardView
+    private lateinit var btnOpciones: MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        btnJugar = findViewById(R.id.btnJugar)
+        btnJugar = findViewById(R.id.cardPlay)
         btnOpciones = findViewById(R.id.btnOpciones)
 
         btnJugar.setOnClickListener {
