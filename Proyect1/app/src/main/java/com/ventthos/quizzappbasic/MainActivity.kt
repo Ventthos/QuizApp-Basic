5630588933312de17e89957c92cb84cc418d9bef
chaan12 <eduardoezequielchanortiz@gmail.com>
2025-02-14 15:18:48 -0600

    Update uno de activity 1

--- Proyect1/app/src/main/java/com/ventthos/quizzappbasic/MainActivity.kt
+++ Proyect1/app/src/main/java/com/ventthos/quizzappbasic/MainActivity.kt
@@ -3,25 +3,25 @@ package com.ventthos.quizzappbasic
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var no: Button
+    lateinit var btnJugar: Button
+    lateinit var btnOpciones: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        no = findViewById(R.id.buttom)
+        btnJugar = findViewById(R.id.btnJugar)
+        btnOpciones = findViewById(R.id.btnOpciones)
 
-        no.setOnClickListener {
+        btnJugar.setOnClickListener {
             val intent = Intent(this, GameScreen::class.java)
             startActivity(intent)
         }
+
     }
 }
